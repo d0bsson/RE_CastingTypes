a90a30900e8bb0def42f6f8ab2b133d4b8f9302b
David <ber.dev@yandex.ru>
2021-09-06 21:43:59 +0300

    create all view

---
+++ RE_CastingTypes/Models/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  RE_CastingTypes
+//
+//  Created by Дэвид Бердников on 06.09.2021.
+//
+
+
+struct Person {
+    let name: String
+    let secondName: String
+    let age: Int
+    let someText: String
+    let hobby: Hobby
+}
+
+struct Hobby {
+    let game: String
+    let experience: Int
+}
